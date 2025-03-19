df60767c152d6e96af3679da191f1487f6194fbb
PlasticFriend <sayan02.official@gmail.com>
2025-03-19 11:31:42 +0530

    test commit

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ mongoose.connect(process.env.MONGODB_URI, {
 .then(() => console.log('MongoDB connected'))
 .catch(err => console.error('MongoDB connection error:', err));
 
-// Routes
+// Routes to be included
 app.use('/api/auth', authRoutes);
 app.use('/api/materials', materialRoutes);
 app.use('/api/users', userRoutes);
